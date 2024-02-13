993faacf95856584c8e6a18bb34f79ab2b3187a6
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-02-13 09:15:55 +0100

    Initial commit

---
+++ lib/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ lib/src/test/java/io/github/pshevche/equinox/LibraryTest.java
@@ -0,0 +1,12 @@
+package io.github.pshevche.equinox;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class LibraryTest {
+    @Test
+    void someLibraryMethodReturnsTrue() {
+        fail();
+    }
+}

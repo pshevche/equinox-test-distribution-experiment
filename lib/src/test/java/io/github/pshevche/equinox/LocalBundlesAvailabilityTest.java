865a1552bebda0cba3a6bdc36295e0c103d9d5a1
Pavlo Shevchenko <pshevchenko@gradle.com>
2024-02-13 11:31:24 +0100

    Add tests for local and remote bundles availability

---
+++ lib/src/test/java/io/github/pshevche/equinox/LocalBundlesAvailabilityTest.java
@@ -0,0 +1,8 @@
+package io.github.pshevche.equinox;
+
+import com.gradle.develocity.testing.annotations.LocalOnly;
+
+@LocalOnly
+class LocalBundlesAvailabilityTest extends BaseBundlesAvailabilityTest {
+
+}
